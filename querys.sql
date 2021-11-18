3e5c60179025e9e77f24dd447ec8bf0cf56a4d87
Christian Herrero Tinoco <57004297+Cherrerotinoco@users.noreply.github.com>
2021-11-18 12:00:42 +0100

    Insert employees

---
+++ querys.sql
@@ -0,0 +1,20 @@
+-- INSERT DATA
+
+/* Add 15 employees */
+INSERT INTO employees (emp_no, birth_date, first_name, last_name, gender, hire_date)
+VALUES 
+(1 , '1996-12-24', 'Christian', 'Herrero', 'M', CURDATE()),
+(2 , '1995-11-11', 'Jose', 'Gonzalez', 'M', CURDATE()),
+(3 , '1995-05-25', 'Lorena', 'Gutierrez', 'F', CURDATE()),
+(4 , '1996-04-06', 'Julia', 'Gonzalez', 'F', CURDATE()),
+(5 , '1996-12-01', 'Alberto', 'Herrero', 'M', CURDATE()),
+(6 , '1994-01-05', 'Jose Manuel', 'Ramirez', 'M', CURDATE()),
+(7 , '1995-11-15', 'Amparo', 'Cardenas', 'F', CURDATE()),
+(8 , '1995-08-21', 'Alan', 'Martinez', 'M', CURDATE()),
+(9 , '1995-10-22', 'Clara', 'Muncilla', 'F', CURDATE()),
+(10 , '1996-09-10', 'Sofia', 'Cardenas', 'F', CURDATE()),
+(11 , '1995-10-11', 'Nuria', 'Cardenas', 'F', CURDATE()),
+(12 , '1994-06-03', 'Matías', 'Garcia', 'M', CURDATE()),
+(13 , '1996-04-01', 'Antonio', 'Herrero', 'M', CURDATE()),
+(14 , '1995-11-11', 'Sonia', 'Gonzalez', 'F', CURDATE()),
+(15 , '1995-11-11', 'Jose', 'Gonzalez', 'M', CURDATE());
